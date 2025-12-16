a344a9f3e607cae4f9cd717adcf45120e042e6c8
tienlm <tienlm@vtc.vn>
2025-12-16 09:52:44 +0700

    wedding invitation

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import "./globals.css";
+import { Playfair_Display, Great_Vibes } from "next/font/google";
+
+const playfair = Playfair_Display({
+    subsets: ["latin"],
+    variable: "--font-playfair",
+});
+
+const greatVibes = Great_Vibes({
+    subsets: ["latin"],
+    weight: "400",
+    variable: "--font-great-vibes",
+});
+
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="vi">
+        <body className={`${playfair.variable} ${greatVibes.variable}`}>
+        {children}
+        </body>
+        </html>
+    );
+}
