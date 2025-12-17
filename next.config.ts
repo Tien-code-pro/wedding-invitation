e8b02ca9d21eabdf473fdc120fc56320570245d2
tienlm <tienlm@vtc.vn>
2025-12-17 11:12:52 +0700

    fix bug build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
-  output: 'export',
+  // output: 'export',
+  output: 'standalone',
   basePath: '', //VD: /bapchampion
   images: {
     remotePatterns: [
